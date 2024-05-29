d169274b2dc6319a3149a50b324cd4f895eca862
mdhenry763 <mdhenry763@gmail.com>
2024-05-29 21:10:50 +0200

    chore(player): updated player movement.

--- Pebbles_Prototype/Assets/Scripts/Pipes/Test/MovementTest.cs
+++ Pebbles_Prototype/Assets/Scripts/Pipes/Test/MovementTest.cs
@@ -7,26 +7,33 @@ using UnityEngine.InputSystem;
 
 public class MovementTest : MonoBehaviour
 {
+    public Camera playerCamera;
+    public float jumpPower = 7f;
+    public float gravity = 10f;
+    
     public PlayerInput input;
-    public Rigidbody playerRB;
     public float playerSpeed = 8f;
-    public float rotateSpeed = 5f;
-    public float jumpForce = 200f;
     public float mouseSensitivity = 100f;
+    
+    float rotationX = 0;
+
+    public bool canMove = true;
 
     public LayerMask fireLayer;
-    public CinemachineVirtualCamera _cinemachineCam;
 
     private Vector3 _moveInput = Vector3.zero;
     private Camera _mainCam;
     private float _rotationX = 0f;
-    private CinemachineComposer _composer;
+    
+    CharacterController characterController;
 
     private void Awake()
     {
         _mainCam = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
-        _composer = _cinemachineCam.GetCinemachineComponent<CinemachineComposer>();
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     private void LateUpdate()
@@ -56,16 +63,17 @@ public class MovementTest : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (!IsGrounded()) return;
-        
-        playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Force);
+        if (characterController.isGrounded)
+        {
+            characterController.Move(new Vector3(0, jumpPower, 0));
+        }
     }
 
     public void OnFire(InputAction.CallbackContext context)
     {
         RaycastHit hit;
 
-        //casts ray to mous position
+        //casts ray to mouse position
         Ray mousePos = _mainCam.ScreenPointToRay(Mouse.current.position.ReadValue());
 
         //if there was a ray cast the player will look at it
@@ -78,42 +86,41 @@ public class MovementTest : MonoBehaviour
 
     private void HandleMovementAndRotation()
     {
-        if(!IsGrounded()) return;
+        if(!canMove) return;
         
         Vector3 moveDir = (transform.forward * _moveInput.z) + (transform.right * _moveInput.x);
-        moveDir.y = playerRB.velocity.y;
-        playerRB.velocity = moveDir * playerSpeed ;
+        characterController.Move(moveDir * playerSpeed);
     }
-
-    public float upDownSensitivity = 0.2f;
     
     private void HandleMouseLook()
     {
+        // Vector2 mouseInput = Mouse.current.delta.ReadValue();
+        // float mouseX = mouseInput.x * mouseSensitivity * Time.deltaTime;
+        // float mouseY = mouseInput.y * mouseSensitivity * Time.deltaTime;
+        //
+        // _rotationX -= mouseY;
+        // _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
+        
+        if(!canMove) return;
+
         Vector2 mouseInput = Mouse.current.delta.ReadValue();
-        float mouseX = mouseInput.x * mouseSensitivity * Time.deltaTime;
-        float mouseY = mouseInput.y * mouseSensitivity * Time.deltaTime;
 
-        _rotationX -= mouseY;
-        _rotationX = Mathf.Clamp(_rotationX, -90f, 90f);
-
-        transform.Rotate(Vector3.up * mouseX);
-
-        if (_composer != null)
-        {
-            _composer.m_TrackedObjectOffset.y = -_rotationX * upDownSensitivity;
-        }
+        rotationX += -mouseInput.y * mouseSensitivity;
+        rotationX = Mathf.Clamp(rotationX, -90, 90);
+        playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, mouseInput.x * mouseSensitivity, 0);
         //_mainCam.transform.localRotation = Quaternion.Euler(_rotationX, 0f, 0f);
     }
 
     public void DisableInput()
     {
-        input.actions.Disable();
+        canMove = false;
         Cursor.lockState = CursorLockMode.None;
     }
 
     public void EnableInput()
     {
-        input.actions.Enable();
+        canMove = true;
         Cursor.lockState = CursorLockMode.Locked;
     }
 
