be77f0da032d58bbe09e4fe0f8c9ad9f5b3460c8
Michael Henry <mdhenry763@gmail.com>
2024-09-19 20:26:34 +0200

    playing around with character controller.

---
+++ Pebbles_Prototype/Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float moveSpeed = 0.1f;
+    public float jumpForce = 10f;
+    public LayerMask groundLayer;
+    public float maxVelMagnitude = 200f;
+    
+    //Player Components
+    private InputActions _controls;
+    private Rigidbody _playerRb;
+    private Transform _camTransform;
+    
+    //State
+    private bool isGrounded;
+
+    private void Awake()
+    {
+        _controls = new InputActions();
+        _playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _camTransform = Camera.main.transform;
+    }
+
+    private void OnEnable()
+    {
+        _controls.Enable();
+        
+        _controls.Player.Jump.performed += HandlePlayerJump;
+    }
+
+    private void OnDisable()
+    {
+        _controls.Disable();
+    }
+
+    private void Update()
+    {
+        
+        //Sphere check
+        isGrounded = Physics.SphereCast(transform.position, 0.5f, Vector3.down, out var raycastHit, 1, groundLayer);
+        Debug.Log($"IsGrounded: {isGrounded}");
+        
+        //Movement
+        var input = _controls.Player.Move.ReadValue<Vector2>();
+        var move = _camTransform.forward * input.y + _camTransform.right * input.x;
+        move.y = 0;
+
+        // if (_playerRb.velocity.magnitude > maxVelMagnitude)
+        // {
+        //     _playerRb.velocity *= maxVelMagnitude / _playerRb.velocity.magnitude;
+        // }
+
+        if(!isGrounded) return;
+        _playerRb.velocity += move * moveSpeed;
+        
+    }
+    
+    private void HandlePlayerJump(InputAction.CallbackContext obj)
+    {
+        if(!isGrounded) return;
+        Debug.Log("Jump now boy");
+        var jumpVel = Vector3.up * jumpForce;
+        _playerRb.velocity += jumpVel;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
+    }
+}
